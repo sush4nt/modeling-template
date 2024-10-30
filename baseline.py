39c9acd342cf42c62165e015e7ec4a64b86dd63a
sush4nt <sushant.kb.patil@gmail.com>
2024-10-30 12:07:47 +0000

    restructuring and updated scripts

--- baseline.py
+++ baseline.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from xgboost import XGBClassifier
+from textwrap import dedent
 import seaborn as sns
 sns.set_style("whitegrid")
 from sklearn.model_selection import GridSearchCV, cross_val_score, KFold, StratifiedKFold, RandomizedSearchCV
@@ -13,74 +14,27 @@ from sklearn.metrics import roc_auc_score, roc_curve, precision_recall_curve, pr
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, mean_absolute_percentage_error
 from sklearn.model_selection import KFold, StratifiedKFold, GridSearchCV, cross_val_score, cross_validate
 
-class BaselineClfModeling(object):
+class BaseModeling:
     def __init__(self, estimator, X_train, y_train, X_test, y_test):
         self.estimator = estimator
         self.X_train = X_train
         self.y_train = y_train
         self.X_test = X_test
         self.y_test = y_test
-        # print("=="*30, self.estimator, "=="*30)s
 
     @staticmethod
     def roundoff(num, precision=3):
         return np.round(num, precision)
-    
+
+    def log_metrics(self, metrics_dict):
+        return {**metrics_dict, 'Model name': self.estimator.__class__.__name__}
+
+class BaselineClfModeling(BaseModeling):
     def fit(self):
         self.estimator.fit(self.X_train, self.y_train)
 
         self.preds = self.estimator.predict(self.X_test)
         self.pred_probs = self.estimator.predict_proba(self.X_test)
-        
-    def evaluate(self):
-        try:
-            self.train_accuracy = BaselineClfModeling.roundoff(accuracy_score(self.y_train, self.estimator.predict(self.X_train)))
-            self.test_accuracy = BaselineClfModeling.roundoff(accuracy_score(self.y_test, self.preds))
-            self.train_f1 = BaselineClfModeling.roundoff(f1_score(self.y_train, self.estimator.predict(self.X_train)))
-            self.test_f1 = BaselineClfModeling.roundoff(f1_score(self.y_test, self.preds))
-            self.precision = BaselineClfModeling.roundoff(precision_score(self.y_test, self.preds))
-            self.recall = BaselineClfModeling.roundoff(recall_score(self.y_test, self.preds))
-            self.roc_auc = BaselineClfModeling.roundoff(roc_auc_score(self.y_test, self.preds))
-            self.c_matrix = confusion_matrix(self.y_test, self.preds)
-            self.classify_report = classification_report(self.y_test, self.preds)
-            print("\n" + "="*100)
-            print(" " * 40 + "LogisticRegression()")
-            print("="*100)
-            
-            print("="*40 + " Model Performance Metrics " + "="*40)
-            print(f"{'Train accuracy:':<25} {self.train_accuracy:.2f}")
-            print(f"{'Test accuracy:':<25} {self.test_accuracy:.2f}")
-            print(f"{'Train F1 Score:':<25} {self.train_f1:.3f}")
-            print(f"{'Test F1 Score:':<25} {self.test_f1:.3f}")
-            print("="*100)
-            
-            print("\n" + "="*40 + " Confusion Matrix " + "="*40)
-            print(self.c_matrix)
-            print("="*100)
-            
-            print("\n" + "="*40 + " Additional Metrics " + "="*40)
-            print(f"{'Precision Score:':<25} {self.precision:.3f}")
-            print(f"{'Recall Score:':<25} {self.recall:.3f}")
-            print(f"{'ROC AUC Score:':<25} {self.roc_auc:.3f}")
-            print("="*100 + "\n")
-            self._plot_roc()
-            
-            print("="*40 + " Classification Report " + "="*40)
-            print(self.classify_report)
-            print("="*100 + "\n")
-            self._plot_pr()
-
-            print("="*100)
-            print("\n" + "="*40 + " Residuals " + "="*40)
-            self._plot_residuals()
-            
-            self.results = self.log_metrics()
-            print("\n" + "="*100)
-            print(" " * 40 + "Metrics Logged")
-            print("="*100)
-        except Exception as err:
-            print("Error: ", err)
-            print("Please checking if model.fit happened correctly")
 
     @staticmethod
     def _calculate_pr_auc(precision, recall):
@@ -110,7 +64,6 @@ class BaselineClfModeling(object):
 
     def _plot_pr(self):
         precisions, recalls, thresholds = precision_recall_curve(self.y_test, self.estimator.predict_proba(self.X_test)[:,1])
-        self.pr_auc = BaselineClfModeling._calculate_pr_auc(precisions, recalls)
         print("Area under P-R Curve: ", self.pr_auc)
         plt.figure(figsize=(5,3))
         plt.plot(recalls, precisions, marker='.', label='AU-PR')
@@ -119,7 +72,6 @@ class BaselineClfModeling(object):
         plt.title('AU-PR')
         plt.legend()
         plt.show()
-
 
         threshold_boundary = thresholds.shape[0]
         plt.figure(figsize=(5,3))
@@ -133,14 +85,6 @@ class BaselineClfModeling(object):
         plt.legend()
         plt.show()
 
-        pr_df = pd.DataFrame({'precision': precisions[0:threshold_boundary],
-                              'recall':recalls[0:threshold_boundary],
-                              'threshold':thresholds
-                             })
-        pr_df['diff'] = np.abs(pr_df['precision']-pr_df['recall'])
-        self.threshold_tuned = pr_df[pr_df['diff']==pr_df['diff'].min()]['threshold'].iloc[0]
-        print("----->   ", "Optimal threshold: ", self.threshold_tuned)
-
     def _plot_residuals(self):
         residuals = self.y_test - self.pred_probs[:, 1]
         plt.figure(figsize=(5,3))
@@ -148,6 +92,75 @@ class BaselineClfModeling(object):
         plt.ylabel("Residuals")
         plt.title("Residuals distribution")
         plt.show()
+        
+    def evaluate(self, verbose=True):
+        try:
+            self.train_accuracy = BaselineClfModeling.roundoff(accuracy_score(self.y_train, self.estimator.predict(self.X_train)))
+            self.test_accuracy = BaselineClfModeling.roundoff(accuracy_score(self.y_test, self.preds))
+            self.train_f1 = BaselineClfModeling.roundoff(f1_score(self.y_train, self.estimator.predict(self.X_train)))
+            self.test_f1 = BaselineClfModeling.roundoff(f1_score(self.y_test, self.preds))
+            self.precision = BaselineClfModeling.roundoff(precision_score(self.y_test, self.preds))
+            self.recall = BaselineClfModeling.roundoff(recall_score(self.y_test, self.preds))
+            self.roc_auc = BaselineClfModeling.roundoff(roc_auc_score(self.y_test, self.preds))
+            self.c_matrix = confusion_matrix(self.y_test, self.preds)
+            self.classify_report = classification_report(self.y_test, self.preds)
+
+            precisions, recalls, thresholds = precision_recall_curve(self.y_test, self.estimator.predict_proba(self.X_test)[:,1])
+            threshold_boundary = thresholds.shape[0]
+            pr_df = pd.DataFrame({'precision': precisions[0:threshold_boundary],
+                                'recall':recalls[0:threshold_boundary],
+                                'threshold':thresholds
+                                })
+            pr_df['diff'] = np.abs(pr_df['precision']-pr_df['recall'])
+            self.threshold_tuned = pr_df[pr_df['diff']==pr_df['diff'].min()]['threshold'].iloc[0]
+            self.pr_auc = BaselineClfModeling._calculate_pr_auc(precisions, recalls)
+
+            if verbose:
+                print(dedent(f"""
+                ====================================================================================================
+                                                        {self.estimator.__class__.__name__}
+                ====================================================================================================
+                ======================================== Model Performance Metrics ========================================
+                Train accuracy:           {self.train_accuracy:.2f}
+                Test accuracy:            {self.test_accuracy:.2f}
+                Train F1 Score:           {self.train_f1:.3f}
+                Test F1 Score:            {self.test_f1:.3f}
+                ====================================================================================================
+
+                ======================================== Confusion Matrix ========================================
+                {self.c_matrix}
+                ====================================================================================================
+
+                ======================================== Additional Metrics ========================================
+                Precision Score:          {self.precision:.3f}
+                Recall Score:             {self.recall:.3f}
+                ROC AUC Score:            {self.roc_auc:.3f}
+                ====================================================================================================
+
+                ======================================== Classification Report ========================================
+                {self.classify_report}
+                ====================================================================================================
+
+                ======================================== Optimal Threshold ========================================
+                {self.threshold_tuned}
+                ====================================================================================================
+                """))
+            
+            self.results = self.log_metrics()
+        except Exception as err:
+            print("Error: ", err)
+            print("Please checking if model.fit happened correctly")
+
+    def display_plots(self):
+        try:
+            print("\n" + "="*40 + " Receiver Operating Characteristis (ROC) " + "="*40)
+            self._plot_roc()
+            print("\n" + "="*40 + " Precision-Recall (PR) Curve " + "="*40)
+            self._plot_pr()
+            print("\n" + "="*40 + " Residuals " + "="*40)
+            self._plot_residuals()
+        except Exception as err:
+                print("Error: ", err)
 
     def log_metrics(self):
         metrics = {
@@ -166,58 +179,10 @@ class BaselineClfModeling(object):
         }
         return metrics
 
-class BaselineRegModeling(object):
-    def __init__(self, estimator, X_train, y_train, X_test, y_test):
-        self.estimator = estimator
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_test = X_test
-        self.y_test = y_test
-
-    @staticmethod
-    def roundoff(num, precision=3):
-        return np.round(num, precision)
-
+class BaselineRegModeling(BaseModeling):
     def fit(self):
         self.estimator.fit(self.X_train, self.y_train)
         self.preds = self.estimator.predict(self.X_test)
-        
-    def evaluate(self):
-        try:
-            self.train_rmse = BaselineRegModeling.roundoff(np.sqrt(mean_squared_error(self.y_train, self.estimator.predict(self.X_train))))
-            self.test_rmse = BaselineRegModeling.roundoff(np.sqrt(mean_squared_error(self.y_test, self.preds)))
-            self.train_mae = BaselineRegModeling.roundoff(mean_absolute_error(self.y_train, self.estimator.predict(self.X_train)))
-            self.test_mae = BaselineRegModeling.roundoff(mean_absolute_error(self.y_test, self.preds))
-            self.train_r2 = BaselineRegModeling.roundoff(r2_score(self.y_train, self.estimator.predict(self.X_train)))
-            self.test_r2 = BaselineRegModeling.roundoff(r2_score(self.y_test, self.preds))
-            self.train_mape = BaselineRegModeling.roundoff(mean_absolute_percentage_error(self.y_train, self.estimator.predict(self.X_train)))
-            self.test_mape = BaselineRegModeling.roundoff(mean_absolute_percentage_error(self.y_test, self.preds))
-            
-            print("\n" + "="*100)
-            print(" " * 40 + f"{self.estimator.__class__.__name__}")
-            print("="*100)
-            
-            print("="*40 + " Model Performance Metrics " + "="*40)
-            print(f"{'Train RMSE:':<25} {self.train_rmse:.3f}")
-            print(f"{'Test RMSE:':<25} {self.test_rmse:.3f}")
-            print(f"{'Train MAE:':<25} {self.train_mae:.3f}")
-            print(f"{'Test MAE:':<25} {self.test_mae:.3f}")
-            print(f"{'Train R^2 Score:':<25} {self.train_r2:.3f}")
-            print(f"{'Test R^2 Score:':<25} {self.test_r2:.3f}")
-            print(f"{'Train MAPE:':<25} {self.train_mape:.3f}")
-            print(f"{'Test MAPE:':<25} {self.test_mape:.3f}")
-            print("="*100)
-            
-            self._plot_residuals()
-            self._plot_predictions()
-            
-            self.results = self.log_metrics()
-            print("\n" + "="*100)
-            print(" " * 40 + "Metrics Logged")
-            print("="*100)
-        except Exception as err:
-            print("Error: ", err)
-            print("Please check if model.fit happened correctly")
 
     def _plot_residuals(self):
         residuals = self.y_test - self.preds
@@ -237,6 +202,47 @@ class BaselineRegModeling(object):
         plt.ylabel("Predicted values")
         plt.title("Actual vs Predicted values")
         plt.show()
+        
+    def evaluate(self, verbose=True):
+        try:
+            self.train_rmse = BaselineRegModeling.roundoff(np.sqrt(mean_squared_error(self.y_train, self.estimator.predict(self.X_train))))
+            self.test_rmse = BaselineRegModeling.roundoff(np.sqrt(mean_squared_error(self.y_test, self.preds)))
+            self.train_mae = BaselineRegModeling.roundoff(mean_absolute_error(self.y_train, self.estimator.predict(self.X_train)))
+            self.test_mae = BaselineRegModeling.roundoff(mean_absolute_error(self.y_test, self.preds))
+            self.train_r2 = BaselineRegModeling.roundoff(r2_score(self.y_train, self.estimator.predict(self.X_train)))
+            self.test_r2 = BaselineRegModeling.roundoff(r2_score(self.y_test, self.preds))
+            self.train_mape = BaselineRegModeling.roundoff(mean_absolute_percentage_error(self.y_train, self.estimator.predict(self.X_train)))
+            self.test_mape = BaselineRegModeling.roundoff(mean_absolute_percentage_error(self.y_test, self.preds))
+            
+            if verbose:
+                print(dedent(f"""
+                ====================================================================================================
+                                                    {self.estimator.__class__.__name__}
+                ====================================================================================================
+                ======================================== Model Performance Metrics ========================================
+                Train RMSE:              {self.train_rmse:.3f}
+                Test RMSE:               {self.test_rmse:.3f}
+                Train MAE:               {self.train_mae:.3f}
+                Test MAE:                {self.test_mae:.3f}
+                Train R^2 Score:         {self.train_r2:.3f}
+                Test R^2 Score:          {self.test_r2:.3f}
+                Train MAPE:              {self.train_mape:.3f}
+                Test MAPE:               {self.test_mape:.3f}
+                ====================================================================================================
+                """))
+            self.results = self.log_metrics()
+        except Exception as err:
+            print("Error: ", err)
+            print("Please check if model.fit happened correctly")
+
+    def display_plots(self):
+        try:
+            print("\n" + "="*40 + " Residuals " + "="*40)
+            self._plot_residuals()
+            print("\n" + "="*40 + " Predictions v/s Actual values " + "="*40)
+            self._plot_predictions()
+        except Exception as err:
+            print("Error: ", err)
 
     def log_metrics(self):
         metrics = {
--- cvmodeling.py
+++ cvmodeling.py
@@ -13,8 +13,18 @@ import matplotlib.pyplot as plt
 # from sklearn.model_selection import KFold, StratifiedKFold, GridSearchCV, cross_val_score, cross_validate
 from baseline import *
 
-class CVModeling(BaselineClfModeling):
-    def __init__(self, estimator, X_train, y_train, X_test, y_test, scoring='f1', cv_type='stratifiedkfold', n_splits=5, param_grid=None):
+class ClassificationCV(BaselineClfModeling):
+    def __init__(
+        self, 
+        estimator, 
+        X_train, 
+        y_train, 
+        X_test, 
+        y_test, 
+        scoring='f1', 
+        cv_type='stratifiedkfold', 
+        n_splits=5, 
+        param_grid=None):
         super().__init__(estimator, X_train, y_train, X_test, y_test)
         self.cv_type = cv_type
         self.n_splits = n_splits
@@ -39,33 +49,96 @@ class CVModeling(BaselineClfModeling):
         elif scoring == 'roc_auc':
             return 'roc_auc'
         elif scoring == 'pr_auc':
-            return make_scorer(CVModeling.pr_auc, needs_proba=True)
+            return make_scorer(ClassificationCV.pr_auc, needs_proba=True)
         else:
             raise "Invalid scoring metric"
     
     def perform_cv(self):
-        scorer = CVModeling.custom_scorer(self.scoring)
+        scorer = ClassificationCV.custom_scorer(self.scoring)
         if self.cv_type == 'kfold':
             cv = KFold(n_splits=self.n_splits, shuffle=self.SHUFFLE, random_state=self.SEED)
-            results = cross_val_score(self.estimator, self.X_train, self.y_train, cv=cv, scoring=scorer, return_train_score=True)
-            print("=="*20, "K-Fold CV Train score:", np.mean(results['train_score']), "=="*20)
-            print("=="*20, "K-Fold CV Test score:", np.mean(results['test_score']), "=="*20)
+            results = cross_validate(self.estimator, self.X_train, self.y_train, cv=cv, scoring=scorer, return_train_score=True)
+            print("=="*20, "K-Fold CV Train score:", "=="*20, "\n", np.mean(results['train_score']))
+            print("=="*20, "K-Fold CV Test score:", "=="*20, "\n", np.mean(results['test_score']))
             return np.mean(results['train_score']), np.mean(results['test_score'])
         elif self.cv_type == 'stratifiedkfold':
             cv = StratifiedKFold(n_splits=self.n_splits, shuffle=self.SHUFFLE, random_state=self.SEED)
             results = cross_validate(self.estimator, self.X_train, self.y_train, cv=cv, scoring=scorer, return_train_score=True)
-            print("=="*20, "Stratified K-Fold CV Train score:", np.mean(results['train_score']), "=="*20)
-            print("=="*20, "Stratified K-Fold CV Test score:", np.mean(results['test_score']), "=="*20)
+            print("=="*20, "Stratified K-Fold CV Train score:", "=="*20, "\n", np.mean(results['train_score']))
+            print("=="*20, "Stratified K-Fold CV Test score:", "=="*20, "\n", np.mean(results['test_score']))
             return np.mean(results['train_score']), np.mean(results['test_score'])
         elif self.cv_type == 'gridsearchcv':
             if self.param_grid is None:
                 raise ValueError("param_grid must be provided for GridSearchCV")
             grid_search = GridSearchCV(self.estimator, self.param_grid, cv=self.n_splits, scoring=scorer)
             grid_search.fit(self.X_train, self.y_train)
-            print("=="*20, "Grid Search CV best cv results: ", grid_search.best_params_, grid_search.best_score_, "=="*20)
-            print("=="*20, "Grid Search CV best estimator: ", grid_search.best_estimator_, "=="*20)
-            print("=="*20, "Grid Search CV best params: ", grid_search.best_params_, "=="*20)
-            print("=="*20, "Grid Search CV best score: ", grid_search.best_score_, "=="*20)
+            print("=="*20, "Grid Search CV best cv results: ", "=="*20, "\n", grid_search.best_params_, grid_search.best_score_)
+            print("=="*20, "Grid Search CV best estimator: ", "=="*20, "\n", grid_search.best_estimator_)
+            print("=="*20, "Grid Search CV best params: ", "=="*20, "\n", grid_search.best_params_)
+            print("=="*20, "Grid Search CV best score: ", "=="*20, "\n", grid_search.best_score_)
             return grid_search.cv_results_
         else:
             raise ValueError(f"Unknown cv_type: {self.cv_type}. Valid types are 'kfold', 'stratifiedkfold', 'gridsearchcv'")
+        
+class RegressionCV(BaselineRegModeling):
+    def __init__(
+        self, 
+        estimator, 
+        X_train, 
+        y_train, 
+        X_test, 
+        y_test, 
+        scoring='r2', 
+        cv_type='kfold', 
+        n_splits=5, 
+        param_grid=None):
+        super().__init__(estimator, X_train, y_train, X_test, y_test)
+        self.cv_type = cv_type
+        self.n_splits = n_splits
+        self.param_grid = param_grid
+        self.scoring = scoring
+        self.SEED = 1234
+        self.SHUFFLE = True
+
+    @staticmethod
+    def custom_scorer(scoring):
+        if scoring == 'neg_mean_squared_error':
+            return make_scorer(mean_squared_error, greater_is_better=False)
+        elif scoring == 'neg_mean_absolute_error':
+            return make_scorer(mean_absolute_error, greater_is_better=False)
+        elif scoring == 'r2':
+            return make_scorer(r2_score)
+        elif scoring == 'neg_mean_absolute_percentage_error':
+            return make_scorer(mean_absolute_percentage_error, greater_is_better=False)
+        else:
+            raise ValueError("Invalid scoring metric")
+
+    def perform_cv(self):
+        scorer = RegressionCV.custom_scorer(self.scoring)
+
+        if self.cv_type == 'kfold':
+            cv = KFold(n_splits=self.n_splits, shuffle=self.SHUFFLE, random_state=self.SEED)
+            results = cross_validate(self.estimator, self.X_train, self.y_train, cv=cv, scoring=scorer, return_train_score=True)
+            if self.scoring=='r2':
+                train_score = np.mean(results['train_score'])
+                test_score = np.mean(results['test_score'])
+            else:
+                train_score = np.mean(-1*results['train_score'])
+                test_score = np.mean(-1*results['test_score'])
+            print("=="*20, "K-Fold CV Train score:", "=="*20, "\n", train_score)
+            print("=="*20, "K-Fold CV Test score:", "=="*20, "\n", test_score)
+            return train_score, test_score
+
+        elif self.cv_type == 'gridsearchcv':
+            if self.param_grid is None:
+                raise ValueError("param_grid must be provided for GridSearchCV")
+            grid_search = GridSearchCV(self.estimator, self.param_grid, cv=self.n_splits, scoring=scorer)
+            grid_search.fit(self.X_train, self.y_train)
+            print("=="*20, "Grid Search CV best cv results: ", "=="*20, "\n", grid_search.best_params_, grid_search.best_score_)
+            print("=="*20, "Grid Search CV best estimator: ", "=="*20, "\n", grid_search.best_estimator_)
+            print("=="*20, "Grid Search CV best params: ", "=="*20, "\n", grid_search.best_params_)
+            print("=="*20, "Grid Search CV best score: ", "=="*20, "\n", grid_search.best_score_)
+            return grid_search.cv_results_
+
+        else:
+            raise ValueError(f"Unknown cv_type: {self.cv_type}. Valid types are 'kfold', 'gridsearchcv'")
